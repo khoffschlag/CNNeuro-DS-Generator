586909b63b8ca4575a00b3558fe467f6ffdaa5bc
Kevin Hoffschlag <72939508+khoffschlag@users.noreply.github.com>
2022-03-23 18:48:22 +0100

    integrate versioneer

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import versioneer
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+with open('requirements.txt', 'r') as file:
+    requires = file.read().splitlines()
+
+setup(
+    name='cnneuro-ds-generator',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='Dataset generator for the CNNeuro project',
+    long_description=long_description,
+    packages=find_packages(include=['cnneuro-ds-generator', 'cnneuro-ds-generator.*']),
+    install_requires=requires,
+    python_requires='>=3.9'
+)
